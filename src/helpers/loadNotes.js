62e689ab3c29fe776a135d9eb2dcabf310b2d06c
Luis Chilo <gfxargentina@gmail.com>
2021-04-09 20:00:14 -0300

    fix loadnotes helper

--- src/helpers/loadNotes.js
+++ src/helpers/loadNotes.js
@@ -1,7 +1,7 @@
 import { db } from "../firebase/firebase-config";
 
 export const loadNotes = async ( uid ) => {
-    const notesSnap = await db.collection(`${ uid }/journal/notes`).get();
+    const notesSnap = await db.collection(`${ uid }/journal/notes`).orderBy("date", "desc").get();
     const notes = [];
 
     //console.log(notesSnap);
